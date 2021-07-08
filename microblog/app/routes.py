0a3b8ae536bc34d7ebdfc528a58bb963481180ba
aparaev <nabrosok-hd@yandex.ru>
2021-07-09 00:36:28 +0700

    l2: add base template

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -2,8 +2,23 @@
 from flask import render_template
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'haruspex'}
-    return render_template('index.html', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        },
+    ]
+    return render_template('index.html', user=user, posts=posts)
