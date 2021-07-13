37e7749620312332ee9f17e8044ae19fcfa71c0b
aparaev <nabrosok-hd@yandex.ru>
2021-07-14 01:56:39 +0700

    database fun

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,3 +1,11 @@
-from app import app
+from app import app, db
+from app.models import User, Post
 
-app.run('127.0.0.1', port=5000, debug=True)
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+
+if __name__ == "__main__":
+    app.run('127.0.0.1', port=5000, debug=True)
